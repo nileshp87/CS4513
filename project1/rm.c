3b07185d1a12d9501a0218956665d810e3e19354
Nilesh Patel <nileshp88@gmail.com>
2014-03-25 03:06:39 -0400

    Path resolution stuff done a bit, env variable processed.

--- project1/rm.c
+++ project1/rm.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h> 
+
+#define _GNU_SOURCE
 
 // Errors
 #define SUCCESS 0
@@ -17,6 +22,10 @@ int rmf(char* file);
 int rmr(char* file);
 int rmrf(char* file);
 void usage(char* name);
+char *toAbsolute(char* file);
+
+char* TRASH;
+char* cwd;
 
 int main(int argc, char* argv[])
 {
@@ -27,18 +36,50 @@ int main(int argc, char* argv[])
 	int i;
 	for(i = 1; i < argc; i++)
 	{
-		if(strcmp("-h",argv[i]) == 0)
+		if(!strcmp("-h",argv[i]))
 			usage(argv[0]);
 		// Prevent weirdness like: "rm -r -f -r -f -f -r" from messing with modes
-		else if(strcmp("-f",argv[i]) == 0)
+		else if(!strcmp("-f",argv[i]))
 			mode += mode%2 == 0 ? FORCE : DEFAULT;
-		else if(strcmp("-r",argv[i]) == 0)
+		else if(!strcmp("-r",argv[i]))
 			mode += mode < 2 ? RECURSIVE : DEFAULT;
 		else
 			break;
 	}
-	
+	// Get the trash variable
+	TRASH = getenv("TRASH");
+	if(!TRASH){
+		puts("Error: TRASH variable not set, please set $TRASH to a created directory.");
+		exit(1);
+	}
+	cwd = get_current_dir_name();
+	printf("CWD: %s\n", cwd);
+	printf("TRASH: %s\n", TRASH);
+	printf("FD: %s\n", *(argv + i));
+	toAbsolute(*(argv + i));
+	rm(*(argv + i));
+}
 
+char *toAbsolute(char* file)
+{
+	if(!strncmp(file,"/",1))
+		puts(file);
+	else
+		printf("%s/%s\n",cwd,file);
+	return file;
+}
+
+int rm(char* file)
+{
+	char* newpath = calloc(strlen(file) + strlen(TRASH) + 1, sizeof(char));
+	strcat(newpath, TRASH);
+	strcat(newpath, file);
+	int x = link(file, newpath);
+	printf("Status: %d\n", x);
+	int errsv = errno;
+	printf("Status: %s\n", strerror(errsv));
+	printf("NP: %s\n", newpath);
+	return x;
 }
 
 void usage(char* name){
